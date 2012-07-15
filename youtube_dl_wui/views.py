c4c0bad6ab476c67304e1f0f9a428a8f61189c95
Nicolas Doualot <public@slubman.info>
2012-07-15 14:59:23 +0200

    Handle form submission
    * Still no action taken.

--- youtube_dl_wui/views.py
+++ youtube_dl_wui/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 from django.template import Context, RequestContext, Template
 from django.shortcuts import render_to_response
@@ -10,6 +11,11 @@ class VideoForm(forms.Form):
 def home(request):
 	if request.method == 'POST':
 		form = VideoForm(request.POST)
+		if form.is_valid():
+			url = form.cleaned_data['url']
+			mail = form.cleaned_data['email']
+			
+			#form = VideoForm()
 	else:
 		form = VideoForm()
 	return render_to_response('index.html', {'video': form}, context_instance=RequestContext(request))
